4daea4b06ff539cc38bab07d20ad02c1494d7f4e
filip <fbielejec@gmail.com>
2022-10-07 15:01:30 +0200

    script to start all

---
+++ start_all_components.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -evx
+
+# start archiver component
+tmux new-session -s indexer -d 'docker-compose -f docker-compose.archive.yml up';
+
+# start db for the squid
+tmux new-window -t "indexer:1";
+tmux send-keys -t "indexer:1" 'docker-compose -f docker-compose.squiddb.yml up' Enter;
+
+# start squid
+tmux new-window -t "indexer:2";
+tmux send-keys -t "indexer:2" 'make build && make process' Enter;
+
+# start API server
+tmux new-window -t "indexer:3";
+tmux send-keys -t "indexer:3" 'make serve' Enter;
+
+tmux a;
+
+exit $?
