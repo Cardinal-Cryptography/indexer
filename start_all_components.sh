15e8286e877bb1f605a573e6d2e85ed554ffb099
filip <fbielejec@gmail.com>
2022-10-11 14:28:32 +0200

    workflow

--- start_all_components.sh
+++ start_all_components.sh
@@ -11,7 +11,7 @@ tmux send-keys -t "indexer:1" 'docker-compose -f docker-compose.squiddb.yml up' 
 
 # start squid
 tmux new-window -t "indexer:2";
-tmux send-keys -t "indexer:2" 'make build && make process' Enter;
+tmux send-keys -t "indexer:2" 'make migrate && make build && make process' Enter;
 
 # start API server
 tmux new-window -t "indexer:3";
