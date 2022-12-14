35272adb54901444a930d85db738b918a8282c83
Mikolaj Gasior <mikolaj@gasior.dev>
2022-12-14 09:28:58 +0100

    Modify workflow to check docker build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,9 @@
 if [[ "${INDEXER_ENV}" == "featurenet" ]]; then
   ADDR_SRC="/squid/lib/metadata/addresses.json"
   ADDR_DST="/squid/lib/addresses/addresses.json"
+  echo "'featurenet' environment is set, checking for addresses.json downloaded from S3..."
   if [[ -f "${ADDR_SRC}" ]]; then
+    echo "Copying ${ADDR_SRC} to ${ADDR_DST}..."
     cp "${ADDR_SRC}" "${ADDR_DST}"
   fi
 fi
