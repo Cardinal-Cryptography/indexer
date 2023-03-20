3beebca1a18b858c55b916d3bca6f82ce1556714
Filip Bielejec <fbielejec@gmail.com>
2023-03-20 19:42:43 +0100

    update env (#10)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-if [[ "${INDEXER_ENV}" == "featurenet" ]]; then
+if [[ "${INDEXER_ENV}" == "devnet" ]]; then
   ADDR_SRC="/squid/lib/metadata/addresses.json"
   ADDR_DST="/squid/lib/addresses/addresses.json"
-  echo "'featurenet' environment is set, checking for addresses.json downloaded from S3..."
+  echo "'${INDEXER_ENV}' environment is set, checking for addresses.json downloaded from S3..."
   if [[ -f "${ADDR_SRC}" ]]; then
     echo "Copying ${ADDR_SRC} to ${ADDR_DST}..."
     cp "${ADDR_SRC}" "${ADDR_DST}"
